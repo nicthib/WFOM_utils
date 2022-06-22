579375d0380bf46edf842ed55b5a568127195502
Nic Thibodeaux <34455769+nicthib@users.noreply.github.com>
2022-06-21 23:22:33 -0400

    Comments

--- src/IDX_H_std.m
+++ src/IDX_H_std.m
@@ -1,10 +1,10 @@
 function IDXout = IDX_H_std(IDX,data)
+% IDXout = IDX_H_std(IDX,data) calculates the silhoutte values for each assignment pixel in IDX using data as a target.
 disp('Calculating silhouette...')
 if numel(size(data)) == 3
     sz = size(data);
     data = reshape(data,[prod(sz(1:2)) sz(3)]);
 end
-%[C,S] = fsvd(data,500,2,1);
 IDX(IDX==0) = NaN;
 data(IDX==0,:) = NaN;
 IDXout = reshape(silhouette(data, IDX(:),'correlation'),[256 256]);
--- src/getwin.m
+++ src/getwin.m
@@ -1,4 +1,5 @@
 function win = getwin(w,sig)
+% win = getwin(w,sig) returns a window with width w and convolved with a gaussian kernel.
 x = -1000:1000;
 if sig > 0
     y = gaussmf(x,[sig 0]);
